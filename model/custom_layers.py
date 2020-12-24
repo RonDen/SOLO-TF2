fc877815e06a25f015ce99412d964c320cf3e1ac
LuoD <3391131052@qq.com>
2020-12-24 22:54:11 +0800

    add ResNet backbone

--- model/custom_layers.py
+++ model/custom_layers.py
@@ -10,6 +10,7 @@
 import tensorflow as tf
 import tensorflow.keras.layers as layers
 from tensorflow.keras import Model
+from tensorflow.python.keras.backend import var
 from tensorflow.python.keras.layers import Layer
 
 
@@ -19,7 +20,7 @@ METHOD_BILINEAR = 'BILINEAR'
 METHOD_AREA = 'AREA'
 
 
-class Conv2dUnit(object):
+class Conv2dUnit(Model):
     """
     Basic Convolution Unit
     """
@@ -60,15 +61,16 @@ class Conv3x3(object):
         if use_dcn:
             self.conv2d_unit = None
         else:
-            self.conv2d_unit = Conv2dUnit(filter2, 3, strides=strides, padding='same', use_bias=False, bn=0, act=None)
+            self.conv2d_unit = Conv2dUnit(filter2, 3, strides=strides, padding='same', use_bias=False, bn=False, activation=None)
         self.bn = layers.BatchNormalization()
         self.act = layers.ReLU()
     
     def __call__(self, x):
         x = self.conv2d_unit(x)
-        x = self.bn
+        x = self.bn(x)
         x = self.act(x)
         return x
+
 
 
 class InstanceNormalization(Layer):
@@ -81,9 +83,58 @@ class InstanceNormalization(Layer):
     
     def build(self, input_shape):
         super(InstanceNormalization, self).build(input_shape)
-        pass
+        shape = (input_shape[-1], )
+        self.gamma = self.add_weight(shape=shape, initializer='ones', name='gamma')
+        self.beta = self.add_weight(shape=shape, initializer='zeros', name='beta')
+    
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    
+    def call(self, x):
+        N, H, W, C = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
+        # 把同一group的元素融合到一起。IN是GN的特例，当num_groups为c时。
+        x_reshape = tf.reshape(x, (N, H * W, C))
+        mean = tf.reduce_mean(x_reshape, axis=1, keepdims=True)
+        t = tf.square(x_reshape - mean)
+        variance = tf.reduce_mean(t, axis=1, keepdims=True)
+        std = tf.sqrt(variance + self.epsilon)
+        outputs = (x_reshape - mean) / std
+        outputs = self.gamma * outputs + self.beta
+        outputs = tf.reshape(outputs, (N, H, W, C))
+        return outputs
 
 
+class GroupNormalization(Layer):
+    """
+    GroupNormalization, output shape (N, H, W, C)
+    """
+    def __init__(self, num_groups, epsilon=1e-9, **kwargs):
+        super(GroupNormalization, self).__init__()
+        self.epsilon = epsilon
+        self.num_groups = num_groups
+
+    def build(self, input_shape):
+        super(GroupNormalization, self).build(input_shape)
+        shape = (input_shape[-1], )
+        self.gamma = self.add_weight(shape=shape, initializer='ones', name='gamma')
+        self.beta = self.add_weight(shape=shape, initializer='zeros', name='beta')
+    
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    
+    def call(self, x):
+        N, H, W, C = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
+        # 把同一group的元素融合到一起。IN是GN的特例，当num_groups为c时。
+        x_reshape = tf.reshape(x, (N, -1, self.num_groups))
+        mean = tf.reduce_mean(x_reshape, axis=1, keepdims=True)
+        t = tf.square(x_reshape - mean)
+        variance = tf.reduce_mean(t, axis=1, keepdims=True)
+        std = tf.sqrt(variance + self.epsilon)
+        outputs = (x_reshape - mean) / std
+        outputs = tf.reshape(outputs, (N, H * W, C))
+        outputs = self.gamma * outputs + self.beta
+        outputs = tf.reshape(outputs, (N, H, W, C))
+        return outputs
 
 class Resize(Model):
     def __init__(self, h, w, method):
@@ -103,3 +154,19 @@ class Resize(Model):
             m = tf.image.ResizeMethod.AREA
         a = tf.image.resize(x, [self.h, self.w], method=m)
         return a
+
+
+if __name__ == "__main__":
+    data1 = tf.random.uniform(shape=(1, 416, 416, 3), minval=-1, maxval=1)
+    conv1 = Conv3x3(64, 2, False)
+    conv2 = Conv3x3(32, 2, False)
+
+    gn1 = GroupNormalization(4)
+    in1 = InstanceNormalization()
+
+    out1 = conv1(data1)
+    out1 = gn1(out1)
+    out2 = conv2(out1)
+    out2 = in1(out2)
+    
+    print(out2.shape)
---
+++ model/resnet.py
@@ -0,0 +1,179 @@
+# coding=utf-8
+# ================================================================
+#
+#   Author      : LuoDeng
+#   Created date: 2020-12-24 22:02:33
+#   Description : ResNet Backbone
+#
+# ================================================================
+
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+from tensorflow.python.keras import activations
+# from model.custom_layers import Conv2dUnit, Conv3x3
+from custom_layers import Conv2dUnit, Conv3x3
+
+
+class ConvBlock(object):
+    '''
+    官方SOLO仓库中，下采样是在中间的3x3卷积层进行，不同于keras的resnet。
+    与IdentityBlock的区别是在跳连中加了一个1x1卷积
+    一个Block内部有3个卷积层，在一个3x3卷积之前加一个1x1卷积，在条连相加之后再激活
+    '''
+    def __init__(self, filters, use_dcn=False, stride=2):
+        super().__init__()
+        super(ConvBlock, self).__init__()
+        filters1, filters2, filters3 = filters
+
+        # 1x1 conv
+        # 3x3 conv bn relu
+        # 1x1 conv
+        self.conv1 = Conv2dUnit(filters1, 1, strides=1, padding='valid', use_bias=False, bn=True, activation='relu')
+        self.conv2 = Conv3x3(filters2, stride, use_dcn)
+        self.conv3 = Conv2dUnit(filters3, 1, strides=1, padding='valid', use_bias=False, bn=True, activation=None)
+
+        self.conv4 = Conv2dUnit(filters3, 1, strides=stride, padding='valid', use_bias=False, bn=True, activation=None)
+        self.act = layers.ReLU()
+
+    def __call__(self, input_tensor):
+        x = self.conv1(input_tensor)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        shortcut = self.conv4(input_tensor)
+        x = layers.add([x, shortcut])
+        x = self.act(x)
+        return x
+
+
+class IdentityBlock(object):
+    """
+    IdentityBlock
+    经典的ResNet中的IdentityBlock，不过区别是跳连在Block内部
+    一个Block内部有3个卷积层，在一个3x3卷积之前加一个1x1卷积，在条连相加之后再激活
+    """
+    def __init__(self, filters, use_dcn=False):
+        super(IdentityBlock, self).__init__()
+        filters1, filters2, filters3 = filters
+
+        self.conv1 = Conv2dUnit(filters1, 1, strides=1, padding='valid', use_bias=False, bn=True, activation='relu')
+        self.conv2 = Conv3x3(filters2, 1, use_dcn)
+        self.conv3 = Conv2dUnit(filters3, 1, strides=1, padding='valid', use_bias=False, bn=True, activation=None)
+
+        self.act = layers.ReLU()
+
+    def __call__(self, input_tensor):
+        x = self.conv1(input_tensor)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = layers.add([x, input_tensor])
+        x = self.act(x)
+        return x
+
+
+class ResNet(object):
+    def __init__(self, depth, use_dcn=False):
+        super(ResNet, self).__init__()
+        assert depth in [50, 101]
+        # stage1
+        # Image size down 2
+        self.conv1 = Conv2dUnit(64, 7, strides=2, padding='same', use_bias=False, bn=True, activation='relu')
+        # Image size down 2
+        self.maxpool = layers.MaxPool2D(pool_size=3, strides=2, padding='same')
+
+        # stage2
+        self.stage2_0 = ConvBlock([64, 64, 256], stride=1)
+        self.stage2_1 = IdentityBlock([64, 64, 256])
+        self.stage2_2 = IdentityBlock([64, 64, 256])
+
+        # stage3
+        self.stage3_0 = ConvBlock([128, 128, 512], stride=1)
+        self.stage3_1 = IdentityBlock([128, 128, 512], use_dcn=use_dcn)
+        self.stage3_2 = IdentityBlock([128, 128, 512], use_dcn=use_dcn)
+        self.stage3_3 = IdentityBlock([128, 128, 512], use_dcn=use_dcn)
+
+        # stage4
+        self.stage4_0 = ConvBlock([256, 256, 1024], use_dcn=use_dcn)
+        k = 21
+        if depth == 50:
+            k = 4
+        self.stage4_layers = []
+        for i in range(k):
+            ly = IdentityBlock([256, 256, 1024], use_dcn=use_dcn)
+            self.stage4_layers.append(ly)
+        self.stage4_last_layer = IdentityBlock([256, 256, 1024], use_dcn=use_dcn)
+
+        # stage5
+        self.stage5_0 = ConvBlock([512, 512, 2048], use_dcn=use_dcn)
+        self.stage5_1 = IdentityBlock([512, 512, 2048], use_dcn=use_dcn)
+        self.stage5_2 = IdentityBlock([512, 512, 2048], use_dcn=use_dcn)
+    
+    def __call__(self, input_tensor):
+        # down 4x
+        x = self.conv1(input_tensor)
+        x = self.maxpool(x)
+
+        # stage2
+        x = self.stage2_0(x)
+        x = self.stage2_1(x)
+        s4 = self.stage2_2(x)
+    
+        # stage3
+        x = self.stage3_0(s4)
+        x = self.stage3_1(x)
+        x = self.stage3_2(x)
+        s8 = self.stage3_3(x)
+    
+        # stage4
+        x = self.stage4_0(s8)
+        for ly in self.stage4_layers:
+            x = ly(x)
+    
+        s16 = self.stage4_last_layer(x)
+        # stage5
+        x = self.stage5_0(s16)
+        x = self.stage5_1(x)
+        s32 = self.stage5_2(x)
+        return [s4, s8, s16, s32]
+
+
+def _test1():
+    x = tf.random.uniform(shape=(1, 416, 416, 3), minval=-1, maxval=1)
+    conv1 = Conv2dUnit(64, 7, strides=2, padding='same', use_bias=False, bn=True, activation='relu')
+    maxpool = layers.MaxPool2D(pool_size=3, strides=2, padding='same')
+    x = conv1(x)
+    print(x.shape)
+    x = maxpool(x)
+    print(x.shape)
+    
+    stage2_0 = ConvBlock([64, 64, 256], stride=1)
+    x = stage2_0(x)
+    print(x.shape)
+    stage2_1 = IdentityBlock([64, 64, 256])
+    x = stage2_1(x)
+    print(x.shape)
+    stage2_2 = IdentityBlock([64, 64, 256])
+    x = stage2_2(x)
+    print(x.shape)
+
+
+def _test_resnet50():
+    x = tf.random.uniform(shape=(1, 416, 416, 3), minval=-1, maxval=1)
+    resnet50 = ResNet(50)
+    s4, s8, s16, s32 = resnet50(x)
+    print(s4.shape, s8.shape, s16.shape, s32.shape)
+
+def _test_resnet101():
+    x = tf.random.uniform(shape=(1, 416, 416, 3), minval=-1, maxval=1)
+    resnet101 = ResNet(101)
+    s4, s8, s16, s32 = resnet101(x)
+    print(s4.shape, s8.shape, s16.shape, s32.shape)
+
+
+if __name__ == "__main__":
+    print("-"*10, 'test1', "-"*10)
+    _test1()
+    print("-"*10, 'test resnet50', "-"*10)
+    _test_resnet50()
+    print("-"*10, 'test_resnet101', "-"*10)
+    _test_resnet101()
+
